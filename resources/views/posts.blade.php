299be8b997e90b0a7b23ef93dafa6c309e5083d4
Mohammad Foroutan <m.foroutan26@yahoo.com>
2022-07-04 13:53:12 +0430

    Episode 07 - Finish

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="/app.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusamus fugit distinctio tempore ad perspiciatis laborum nihil velit, beatae obcaecati possimus facilis, culpa quibusdam consequuntur incidunt veritatis totam, cumque sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusamus fugit distinctio tempore ad perspiciatis laborum nihil velit, beatae obcaecati possimus facilis, culpa quibusdam consequuntur incidunt veritatis totam, cumque sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusamus fugit distinctio tempore ad perspiciatis laborum nihil velit, beatae obcaecati possimus facilis, culpa quibusdam consequuntur incidunt veritatis totam, cumque sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusamus fugit distinctio tempore ad perspiciatis laborum nihil velit, beatae obcaecati possimus facilis, culpa quibusdam consequuntur incidunt veritatis totam, cumque sequi.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusamus fugit distinctio tempore ad perspiciatis laborum nihil velit, beatae obcaecati possimus facilis, culpa quibusdam consequuntur incidunt veritatis totam, cumque sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusamus fugit distinctio tempore ad perspiciatis laborum nihil velit, beatae obcaecati possimus facilis, culpa quibusdam consequuntur incidunt veritatis totam, cumque sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusamus fugit distinctio tempore ad perspiciatis laborum nihil velit, beatae obcaecati possimus facilis, culpa quibusdam consequuntur incidunt veritatis totam, cumque sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusamus fugit distinctio tempore ad perspiciatis laborum nihil velit, beatae obcaecati possimus facilis, culpa quibusdam consequuntur incidunt veritatis totam, cumque sequi. 
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusamus fugit distinctio tempore ad perspiciatis laborum nihil velit, beatae obcaecati possimus facilis, culpa quibusdam consequuntur incidunt veritatis totam, cumque sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusamus fugit distinctio tempore ad perspiciatis laborum nihil velit, beatae obcaecati possimus facilis, culpa quibusdam consequuntur incidunt veritatis totam, cumque sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusamus fugit distinctio tempore ad perspiciatis laborum nihil velit, beatae obcaecati possimus facilis, culpa quibusdam consequuntur incidunt veritatis totam, cumque sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusamus fugit distinctio tempore ad perspiciatis laborum nihil velit, beatae obcaecati possimus facilis, culpa quibusdam consequuntur incidunt veritatis totam, cumque sequi. 
+        </p>
+    </article>
+</body>
+</html>
