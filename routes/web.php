72bb707b092b83485fb56bd7ffefb2f73f91dc56
m-foroutan <m.foroutan26@yahoo.com>
2022-07-09 01:27:05 +0430

    Episode 25 - Finish

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
@@ -25,4 +26,10 @@ Route::get('posts/{post:slug}', function (Post $post) { // Post:where('slug', $p
     return view('post', [
         'post' =>  $post
     ]);  
+});
+
+Route::get('categories/{category:slug}', function (Category $category){
+    return view('posts',[
+        'posts' => $category->posts
+    ]);
 });
