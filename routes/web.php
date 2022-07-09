08254db7d5afbbccf33c178c1b314567fd327adf
m-foroutan <m.foroutan26@yahoo.com>
2022-07-09 14:28:51 +0430

    Episode 29 - Finish

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' =>  Post::with('category')->get()
+        'posts' =>  Post::latest()->with('category', 'author')->get()
     ]);
 });
 
@@ -31,5 +32,11 @@ Route::get('posts/{post:slug}', function (Post $post) { // Post:where('slug', $p
 Route::get('categories/{category:slug}', function (Category $category){
     return view('posts',[
         'posts' => $category->posts
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author){
+    return view('posts',[
+        'posts' => $author->posts
     ]);
 });
