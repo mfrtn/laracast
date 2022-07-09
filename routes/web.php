ad5449622a53ac6b52dca46ccf27492b7be56278
m-foroutan <m.foroutan26@yahoo.com>
2022-07-09 21:20:52 +0430

    Episode 35 - Finish

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
         'posts' =>  Post::latest()->get(),
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) { // Post:where('slug', $post)->firstORFail()
     // Find a post by its slug and pass it to a view called "post"
@@ -36,7 +36,7 @@ Route::get('categories/{category:slug}', function (Category $category){
         'currentCategory' => $category,
         'categories' => Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author){
     return view('posts',[
