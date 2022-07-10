79680cfcb8bbf7375edcad95b3af40b24e93e38a
m-foroutan <m.foroutan26@yahoo.com>
2022-07-10 21:29:48 +0430

    Episode 44 - Finish

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -9,12 +9,20 @@
         </button>
     </x-slot>
 
-    {{-- <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item> --}}
-    <x-dropdown-item href="/" :active='(request()->category == null )'>All</x-dropdown-item>
+    {{-- <x-dropdown-item
+        href="/?{{ http_build_query(request()->except('category','page')) }}"
+        :active="request()->routeIs('home')"
+        >All</x-dropdown-item> --}}
+    {{-- My Solution --}}
+    <x-dropdown-item
+        href="/?{{ http_build_query(request()->except('category','page')) }}"
+        :active='(request()->category == null )'
+        >All</x-dropdown-item>
+    {{-- <x-dropdown-item href="/?" :active='(request()->category == null )'>All</x-dropdown-item> --}}
 
     @foreach ($categories as $category)
         <x-dropdown-item
-            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category','page')) }}"
             :active='(request()->category == $category->slug)'            
         >{{ ucwords($category->name) }}
         </x-dropdown-item>
