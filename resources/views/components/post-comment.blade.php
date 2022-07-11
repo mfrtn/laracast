db4debabf47c8309836740fd7569522be5c1650a
m-foroutan <m.foroutan26@yahoo.com>
2022-07-11 23:57:50 +0430

    Episode 52  - Finish

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,19 @@
+<article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img class="rounded-xl" src="https://i.pravatar.cc/60" alt="" width="60" height="60">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis voluptas quibusdam esse cum impedit. Rem, voluptatem corporis. Molestias quidem exercitationem quasi odit officiis nesciunt sed odio, at, cum id et?
+        </p>
+    </div>
+</article>
