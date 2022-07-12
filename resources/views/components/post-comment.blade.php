881677736858ef7859c9e243052323b71354e710
Mohammad <m.foroutan26@yahoo.com>
2022-07-12 14:55:01 +0430

    Episode 55 - Finish

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,21 +1,23 @@
 @props(['comment']) {{-- Now we get comment variable --}}
 
-<article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
-    <div class="flex-shrink-0">
-        <img class="rounded-xl" src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60">
-    </div>
+<x-panel class="bg-gray-50">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <img class="rounded-xl" src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60">
+        </div>
 
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">{{ $comment->author->username }}</h3>
-            <p class="text-xs">
-                Posted
-                <time>{{ $comment->created_at }}</time>
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->username }}</h3>
+                <p class="text-xs">
+                    Posted
+                    <time>{{ $comment->created_at }}</time>
+                </p>
+            </header>
+
+            <p>
+                {{ $comment->body }}
             </p>
-        </header>
-
-        <p>
-            {{ $comment->body }}
-        </p>
-    </div>
-</article>
+        </div>
+    </article>
+</x-panel>
