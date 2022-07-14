ceaad388100bc5796189dcdd340f385b3ce830d2
m-foroutan <m.foroutan26@yahoo.com>
2022-07-14 21:31:56 +0430

    Episode 65 - Finish

---
+++ resources/views/components/form/textarea.blade.php
@@ -0,0 +1,9 @@
+@props(['name'])
+
+<x-form.field>
+    <x-form.label name="{{ $name }}" />
+
+    <textarea class="border border-gray-400 p-2 w-full" name="excerpt" id="excerpt" required>{{ old('excerpt') }}</textarea>
+
+    <x-form.error name="{{ $name }}" />
+</x-form.field>
