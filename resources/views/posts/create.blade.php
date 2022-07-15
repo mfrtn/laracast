b69b8522d4624ff62d000a448d0101c86f486241
m-foroutan <m.foroutan26@yahoo.com>
2022-07-15 13:01:23 +0430

    Episode 66 - Behind scenes

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,8 +5,8 @@
         <form method="POST" action="/admin/posts" enctype="multipart/form-data">
             @csrf
 
-            <x-form.input name="title" />
-            <x-form.input name="slug" />
+            <x-form.input name="title" type="text" />
+            <x-form.input name="slug" type="text" />
             <x-form.input name="thumbnail" type="file" />
             <x-form.textarea name="excerpt" />
             <x-form.textarea name="body" />
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,7 +1,6 @@
 <x-layout>
     <section class="px-6 py-8">
         <main class="max-w-lg mx-auto mt-10 bg-gray-100">
-
             <x-panel>
 
                 <h1 class="text-center font-bold text-xl">Login!</h1>
@@ -9,10 +8,10 @@
                 <form method="POST" action="/login" class="mt-10">
                     @csrf
 
-                        <x-form.input name="email" type="email" autocomplete="username" />
-                        <x-form.input name="password" type="password" autocomplete="new-password" />
+                    <x-form.input name="email" type="email" autocomplete="username" />
+                    <x-form.input name="password" type="password" autocomplete="new-password" />
 
-                        <x-form.button>Login</x-form.button>
+                    <x-form.button>Login</x-form.button>
 
                     @error('erorrlogin')
                         <p class="w-4/8 m-auto text-center mt-4 text-red-500">{{ $message }}</p>
